852aebef8050a73a268984b401032817de178a8f
Ron Arts <ron.arts@gmail.com>
2016-07-02 15:23:43 +0200

    First version.

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM postgres:9.6
+MAINTAINER Ron Arts <ron.arts@p711.net>
+
+#RUN locale-gen en_US.UTF-8  
+#ENV LANG=en_US.UTF-8 LANGUAGE=en_US:en LC_ALL=en_US.UTF-8  
+
+ADD init-user-db.sh /docker-entrypoint-initdb.d/init-user-db.sh
+
---
+++ init-user-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+psql -a -v ON_ERROR_STOP=1 --username "${POSTGRES_USER:-postgres}" <<-EOSQL
+    -- Create Gitlab stuff
+    CREATE ROLE gitlab with LOGIN CREATEDB PASSWORD '${GITLAB_PASSWORD}';
+    CREATE DATABASE gitlabhq_production;
+    GRANT ALL PRIVILEGES ON DATABASE gitlabhq_production to gitlab;
+
+    -- Create Odoo stuff
+
+EOSQL
+
+psql -a -v ON_ERROR_STOP=1 --username "${POSTGRES_USER:-postgres}" gitlabhq_production <<-EOSQL
+    create extension pg_trgm;
+EOSQL
+
+
