58204785d87f491f5b1713c7cffef69e382e7202
Ron Arts <ron.arts@gmail.com>
2016-07-10 16:14:52 +0200

    Added Odoo and Mattermost database role creation.

--- init-user-db.sh
+++ init-user-db.sh
@@ -7,8 +7,13 @@ psql -a -v ON_ERROR_STOP=1 --username "${POSTGRES_USER:-postgres}" <<-EOSQL
     CREATE DATABASE gitlabhq_production;
     GRANT ALL PRIVILEGES ON DATABASE gitlabhq_production to gitlab;
 
+    -- Create Mattermost stuff
+    CREATE ROLE mattermost with LOGIN CREATEDB PASSWORD '${GITLAB_PASSWORD}';
+    CREATE DATABASE mattermost_production;
+    GRANT ALL PRIVILEGES ON DATABASE mattermost_production to mattermost;
+
     -- Create Odoo stuff
-    -- will be done later
+    CREATE ROLE odoo with LOGIN CREATEDB PASSWORD '${ODOO_PASSWORD}';
 EOSQL
 
 psql -a -v ON_ERROR_STOP=1 --username "${POSTGRES_USER:-postgres}" gitlabhq_production <<-EOSQL
